81a22cfc136c8905a4678d4710bdfefeb146baf2
Danialefar <enc_danielfarias@hotmail.com.ar>
2023-09-19 17:52:00 -0300

    Initial commit

---
+++ src/g2ej4/Categoria.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package g2ej4;
+
+/**
+ *
+ * @author 20352555674
+ */
+public enum Categoria {
+    COMESTIBLE,
+    LIMPIEZA,
+    PREFUMERIA;
+    
+}
